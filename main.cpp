f30f73be41252b068b0b3566d4e763101cc5afad
SKuprasz <226289@student.pwr.edu.pl>
2018-03-05 22:52:20 +0100

    second commit

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 struct lista		//elementy listy
 {
 	lista * next;
-	int piorytet;
+	int priorytet;
 	int dana;
 };
 
@@ -20,7 +20,8 @@ private:
 public:
 	Kolejka();	//konstruktor
 	~Kolejka();	//destruktor
-
+    bool empty(); //funkcja sprawdza czy pusta
+    void push(int p, int v); //funckja dodaje do kolejki
 };
 
 //**********************************//
@@ -35,15 +36,45 @@ Kolejka::Kolejka()
 //DESTRUKTOR
 Kolejka::~Kolejka()
 {
-
+ //Pozniej dodam jak napisze pop
 }
 
+bool Kolejka::empty()
+{
+    if(!head)
+        return true;
+    else
+        return false;
+}
+
+void Kolejka::push(int pr, int v)
+{
+ /*
+    lista *p , *r;
+    p = new lista;
+    p->next = NULL;
+    p->priorytet = pr;
+    p->dana = v;
+
+    if(!head) head = tail = p; //jesli jest pusta to wstawiamy
+    else if(head->pr < priorytet)
+    {
+        //czy najwyzszy priorytet i wstawiamy
+    }
+    else
+    {
+        //sprawdzamy nizsze priorytety i wstawiamy przed element o nizszym niz nasz
+    }
+    */
+}
 
 
 int main()
 {
 	cout << "KOLEJKA PRIORYTETOWA" << endl;
 
+    Kolejka K;
+    if(K.empty()==true) cout<<"pusta"<<endl;
 
 
 
