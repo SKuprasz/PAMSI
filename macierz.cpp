5aec6da0c47f45590f05d357ce272e4845630cfc
SKuprasz <226289@student.pwr.edu.pl>
2018-05-14 14:22:37 +0200

    dodanie czesci listy

--- macierz.cpp
+++ macierz.cpp
@@ -67,6 +67,101 @@ void Macierz::wyswietl()
     }
 }
 
+
+class Lista
+{
+public:
+    struct Element
+    {
+        int wierzcholek;
+        int waga;
+        Element *nastepny;
+    };
+
+    Element **lista;
+    int liczbaWierzcholkow;
+
+    Lista(int liczbaWierzcholkow); //konstruktor
+    ~Lista();                     //destruktor
+    int LiczbaWierzcholkow();       //zwraca liczbe wierzcholkow
+    int zwrocWaga(int v1,int v2);   //zwraca wage
+    void dodajWierzcholek(int v1, int v2, int waga); //dodaje wierzhcolek
+    void wyswietl();    //wyswietla
+
+
+};
+
+Lista::Lista(int liczbaWierzcholkow)
+{
+
+}
+
+Lista::~Lista()
+{
+
+}
+
+int Lista::LiczbaWierzcholkow()
+{
+    return liczbaWierzcholkow;
+}
+
+int Lista::zwrocWaga(int v1,int v2)
+{
+    if(lista[v1] == NULL)
+    {
+        return 0;           //jesli nie ma elementu
+    }
+    else                    //jesli jest
+    {
+        bool jest = false; //ustawiaomy na false
+        Element *next = lista[v1];  //elementy
+        Element *position = NULL;
+        do
+        {
+            position = next;
+            next = next->nastepny;
+            if(position->wierzcholek == v2)
+            {
+                jest = true;
+                break;
+            }
+        } while (next!=NULL); //do kiedy nastepny nie null
+
+        if(jest) return position -> waga; //zwroc wage
+        else return 0;
+
+    }
+}
+
+void Lista::dodajWierzcholek(int v1, int v2, int waga)
+{
+
+}
+
+void Lista::wyswietl()
+{
+    cout<<"Reprezentacja listty sasiedztwa"<<endl;
+    for(int i=0; i<liczbaWierzcholkow;i++)
+    {
+        if(lista[i] != NULL)
+        {
+            Element *next = lista[i]; //elementy
+            Element *position = NULL;
+            do
+            {
+                position = next;
+                next = next->nastepny; //ustawiony na nastepny w petli
+                cout << i << "->";
+                cout<<position->wierzcholek<<" $ "<<position->waga<<" "; //wyswietlenie wagi i wierzcholka
+            } while(next != NULL);  //do kiedy nastepny nie jest null
+        }
+        cout<<endl;
+    }
+}
+
+
+
 int main()
 {
     Macierz(5).wyswietl();
