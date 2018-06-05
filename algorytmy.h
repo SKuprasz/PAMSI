882f2a69d77686633344f55d4be990f7b6bb98d4
SKuprasz <226289@student.pwr.edu.pl>
2018-06-05 12:44:32 +0200

    poczatek

---
+++ algorytmy.h
@@ -0,0 +1,14 @@
+#ifndef ALGORYTMY_H_INCLUDED
+#define ALGORYTMY_H_INCLUDED
+
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <functional>
+#include "strutury.h"
+
+using namespace std;
+
+
+
+#endif // ALGORYTMY_H_INCLUDED
